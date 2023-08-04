e6f943fee64aba2459a9414828ac2a5bf2ef6eba
William Ngo <88278313+JefJefHoorraayy@users.noreply.github.com>
2023-08-04 10:15:03 +0800

    Add files via upload

--- proj.js
+++ proj.js
@@ -4,32 +4,57 @@ function includeHTML(file, elementId) {
     .then(response => response.text())
     .then(content => {
       document.getElementById(elementId).innerHTML = content;
+      document.getElementById(elementId).classList.add('show');
+
+      // Get all the button elements and their respective dropdown symbols
+      const buttons = document.querySelectorAll('.custom-dropdown-btn');
+
+      // Add a click event listener to each button
+      buttons.forEach((button) => {
+        const dropdownSymbol = button.querySelector('.dropdown-symbol');
+        let rotated = false;
+
+        button.addEventListener('click', () => {
+          // Toggle the rotated flag for the specific button
+          rotated = !rotated;
+
+          // Apply the appropriate rotation class based on the rotated flag
+          if (rotated) {
+            dropdownSymbol.classList.add('rotated');
+            dropdownSymbol.classList.remove('rotated-back');
+          } else {
+            dropdownSymbol.classList.add('rotated-back');
+            dropdownSymbol.classList.remove('rotated');
+          }
+        });
+      });
     });
 }
 
 // Include the header
-includeHTML('header.html', 'header');
-
+includeHTML('templates/header.html', 'header');
 // Include the footer
-includeHTML('footer.html', 'footer');
+includeHTML('templates/footer.html', 'footer');
 
-// Get all the button elements and their respective dropdown symbols
-const buttons = document.querySelectorAll('.custom-dropdown-btn');
 
-// Add a click event listener to each button
-buttons.forEach((button) => {
-  const dropdownSymbol = button.querySelector('.dropdown-symbol');
-  let rotated = false;
+function updateContent() {
+  var contentDisplay = document.getElementById("contentDisplay");
+  var radio1 = document.getElementById("btnradio1");
+  var radio2 = document.getElementById("btnradio2");
 
-  button.addEventListener('click', () => {
-    // Toggle the rotated flag for the specific button
-    rotated = !rotated;
+  if (radio1.checked) {
+    contentDisplay.classList.remove('show');
+    includeHTML('CMECE_copy.html', 'contentDisplay');
+  } else if (radio2.checked) {
+    contentDisplay.classList.remove('show');
+    includeHTML('CMCPE_copy.html', 'contentDisplay');
+  }
+}
 
-    // Apply the appropriate rotation class based on the rotated flag
-    if (rotated) {
-      dropdownSymbol.classList.add('rotated');
-    } else {
-      dropdownSymbol.classList.remove('rotated');
-    }
-  });
+// Call the function once on page load to initialize the content
+updateContent();
+
+// Attach the function to the change event of the radio buttons
+document.querySelectorAll('input[name="btnradio"]').forEach(function(radio) {
+  radio.addEventListener('change', updateContent);
 });
